931f40c168214234485f750b54fb18805f186cce
Tomas <tomasdaniel09@hotmail.com>
2019-05-18 23:35:49 -0400

    Made defaults constants for text and color. In the future woould be better in a default class. Added working of the next button, Added colored text if answer good or wrong. Added resetToDefaultState(). Thank you Sarah ❣️

--- Korean-iOS/Korean-iOS/EnglishToKoreanUIViewController.swift
+++ Korean-iOS/Korean-iOS/EnglishToKoreanUIViewController.swift
@@ -11,25 +11,40 @@ import UIKit
 class EnglishToKoreanUIViewController: UIViewController {
 	@IBOutlet weak var outputLabel: UILabel!
 	@IBOutlet weak var inputTextFieldEnglishToKorean: UITextField!
+	@IBOutlet weak var acceptButton: UIButton!
+	
+	var randIntNumber: Int = 666 //Default value for randIntNumber is 666
+	let defaultTextColor = UIColor.black
+	let textColorGood = UIColor.green
+	let textColorWrong = UIColor.red
+	let nextTitleButton = "Next"
+	let defaultTitleButton = "Accept"
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		showKoreanRandomNum()
 		inputTextFieldEnglishToKorean.delegate = self
 		// Do any additional setup after loading the view.
 	}
 	
 	@IBAction func acceptEnteredText(_ sender: Any) {
 		let inputText = inputTextFieldEnglishToKorean.text!
-		checkAnswerEnglishToKoreanNumbers(randNumber: showKoreanRandomNum(), choice: inputText)
+		//Check if current title is Next or Check
+		if(acceptButton.currentTitle == nextTitleButton) {
+			resetToDefaultState()
+			showKoreanRandomNum()
+		} else {
+			checkAnswerEnglishToKoreanNumbers(randNumber: randIntNumber, choice: inputText)
+		}
 	}
 	
-	func showKoreanRandomNum() -> Int {
+	func showKoreanRandomNum() {
 		let numbersCreator = numbersCreatorFunctions()
 		let randNumber = numbersCreator.randomNumber()
-		let koNumber = numbersCreator.numberDecimaltoStringKorean(decimalNumber: randNumber)
-		outputLabel.text = koNumber
-		return randNumber
+		// Return Label and button to default colours
+		outputLabel.text = String(randNumber)
+		randIntNumber = randNumber
 	}
 	
 	func checkAnswerEnglishToKoreanNumbers(randNumber: Int, choice: String) {
@@ -38,17 +53,29 @@ class EnglishToKoreanUIViewController: UIViewController {
 		let koNumber = numbersCreator.numberDecimaltoStringKorean(decimalNumber: randNumber)
 		var choice = " "
 		choice = inputTextFieldEnglishToKorean.text!
-		if(choice == String(randNumber)) {
+		if(choice == String(koNumber)) {
 			outputLabel.text = "That was the good answer"
+			outputLabel.textColor = textColorGood
+			acceptButton.setTitleColor(textColorGood, for: .normal)
+			acceptButton.setTitle(nextTitleButton, for: .normal)
 		} else {
 			let returnWrongAnswer: String = "The good answer for \(koNumber) was \(randNumber)"
 			outputLabel.text = returnWrongAnswer
+			outputLabel.textColor = textColorWrong
+			acceptButton.setTitleColor(textColorWrong, for: .normal)
+			acceptButton.setTitle(nextTitleButton, for: .normal)
 		}
+	}
+	
+	func resetToDefaultState() {
+		// Wil reset UI to default state in colors and text
+		acceptButton.setTitle(defaultTitleButton, for: .normal)
+		acceptButton.setTitleColor(defaultTextColor, for: .normal)
+		outputLabel.textColor = defaultTextColor
 	}
 }
 
 extension EnglishToKoreanUIViewController : UITextFieldDelegate {
-	
 	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
 		textField.resignFirstResponder()
 		return true
