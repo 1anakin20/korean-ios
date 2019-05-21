8487c32ec853c756621129b6e36c6a1fd17c8f2f
Tomas <tomasdaniel09@hotmail.com>
2019-05-21 00:10:02 -0400

    Added random grandma images when the answer is correct

---
+++ Korean-iOS/Korean-iOS/classes/images.swift
@@ -0,0 +1,13 @@
+//
+//  images.swift
+//  Korean-iOS
+//
+//  Created by Tomas Daniel Nieto on 2019-05-20.
+//  Copyright © 2019 Tomas Daniel Nieto. All rights reserved.
+//
+
+import UIKit
+
+class images: NSObject {
+
+}
