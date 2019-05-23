6b9bf5d16957bc1ab3201d6f40f354667bf91bb5
Tomas <tomasdaniel09@hotmail.com>
2019-05-23 11:04:02 -0400

    new UI working

---
+++ Korean-iOS/Korean-iOS/UIViewControllers/PlayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayViewController.swift
+//  Korean-iOS
+//
+//  Created by Tomas Daniel Nieto on 2019-05-22.
+//  Copyright © 2019 Tomas Daniel Nieto. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
