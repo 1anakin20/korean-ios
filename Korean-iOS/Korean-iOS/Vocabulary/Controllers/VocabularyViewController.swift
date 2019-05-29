be6d097b6ee06411d1546a324b7c56687b36a373
Tomas <tomasdaniel09@hotmail.com>
2019-05-28 22:06:29 -0400

    Added Json parser

---
+++ Korean-iOS/Korean-iOS/Vocabulary/Controllers/VocabularyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VocabularyViewController.swift
+//  Korean-iOS
+//
+//  Created by Tomas Daniel Nieto on 2019-05-28.
+//  Copyright © 2019 Tomas Daniel Nieto. All rights reserved.
+//
+
+import UIKit
+
+class VocabularyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
